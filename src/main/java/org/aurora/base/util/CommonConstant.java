5a8cd22c15f854c2400914ddca82605604dfe146
Aurora-works <e.works.aurora@gmail.com>
2024-08-25 17:29:17 +0800

    feat

--- src/main/java/org/aurora/base/util/CommonConstant.java
+++ src/main/java/org/aurora/base/util/CommonConstant.java
@@ -3,20 +3,20 @@ package org.aurora.base.util;
 /**
  * 公共常量
  */
-public class CommonConstant {
+public interface CommonConstant {
 
     /**
      * 不记录日志的请求参数
      */
-    public static final String[] NO_LOG_REQUEST_PARAMS = {"password", "oldPassword", "newPassword"};
+    String[] NO_LOG_REQUEST_PARAMS = {"password", "oldPassword", "newPassword"};
 
     /**
      * <a href="https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types">常见 MIME 类型列表</a>
      */
-    public static final String MIME_TYPE_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    String MIME_TYPE_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
     /**
      * 定时任务 SystemMonitor 使用的 Redis Key
      */
-    public static String TASK_REDIS_KEY_SYSTEM_MONITOR = "task:system:monitor";
+    String TASK_REDIS_KEY_SYSTEM_MONITOR = "task:system:monitor";
 }
